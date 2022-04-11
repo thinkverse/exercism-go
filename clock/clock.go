b6b7cd4727e95c21b5a11de5d1855af55535985a
thinkverse <hallberg.kim@gmail.com>
2022-04-11 11:40:55 +0200

    Update `Clock` exercise with solution

--- clock/clock.go
+++ clock/clock.go
@@ -1,19 +1,41 @@
 package clock
 
+import "fmt"
+
 // Define the Clock type here.
+type Clock struct {
+	hours   int
+	minutes int
+}
 
 func New(h, m int) Clock {
-	panic("Please implement the New function")
+	minutes := m % 60
+
+	if minutes < 0 {
+		minutes += 60
+		h--
+	}
+
+	hours := (h + (m / 60)) % 24
+
+	if hours < 0 {
+		hours += 24
+	}
+
+	return Clock{
+		hours,
+		minutes,
+	}
 }
 
 func (c Clock) Add(m int) Clock {
-	panic("Please implement the Add function")
+	return New(c.hours, (c.minutes + m))
 }
 
 func (c Clock) Subtract(m int) Clock {
-	panic("Please implement the Subtract function")
+	return New(c.hours, (c.minutes - m))
 }
 
 func (c Clock) String() string {
-	panic("Please implement the String function")
+	return fmt.Sprintf("%.2d:%.2d", c.hours, c.minutes)
 }
