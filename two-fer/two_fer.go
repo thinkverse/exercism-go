1d1a1c45c19f21937c60a66f66c63c455a1cf52d
thinkverse <hallberg.kim@gmail.com>
2022-04-12 01:57:16 +0200

    Update `Two Fer` with faster version

--- two-fer/two_fer.go
+++ two-fer/two_fer.go
@@ -1,11 +1,16 @@
 package twofer
 
-import "fmt"
-
+// Iteration 1 with fmt.Sprintf:
+//
+// 1967450 ~629 ns/op 6 allocs/op
+//
+// Iteration 2 without fmt.Sprintf:
+//
+// 8778876 ~138 ns/op 0 allocs/op
 func ShareWith(name string) string {
 	if name == "" {
-        name = "you"
-    }
-    
-	return fmt.Sprintf("One for %s, one for me.", name)
+		name = "you"
+	}
+
+	return "One for " + name + ", one for me."
 }
