665473501ef5c29676a117b1794199a04d08aa0d
thinkverse <hallberg.kim@gmail.com>
2022-04-12 13:39:15 +0200

    Update `Space Age` with solution

--- space-age/space_age.go
+++ space-age/space_age.go
@@ -2,6 +2,28 @@ package space
 
 type Planet string
 
+// Age returns your age on a planet.
+//
+// Benchmark  35714338  ~31 ns/op
 func Age(seconds float64, planet Planet) float64 {
-	panic("Please implement the Age function")
+	t := float64(31556700)
+
+	switch planet {
+	case "Mercury":
+		t *= 0.2408467
+	case "Venus":
+		t *= 0.61519726
+	case "Mars":
+		t *= 1.8808158
+	case "Jupiter":
+		t *= 11.862615
+	case "Saturn":
+		t *= 29.447498
+	case "Uranus":
+		t *= 84.016846
+	case "Neptune":
+		t *= 164.79132
+	}
+
+	return seconds / t
 }
