c626d4fbad49607cc53a4a44cbf3499880328e2e
Kim Hallberg <hallberg.kim@gmail.com>
2023-03-15 17:31:56 +0100

    Complete `Resistor Color` exercise

--- resistor-color/resistor_color.go
+++ resistor-color/resistor_color.go
@@ -1,11 +1,40 @@
 package resistorcolor
 
+import "sort"
+
+var colors = map[string]int{
+	"black":  0,
+	"brown":  1,
+	"red":    2,
+	"orange": 3,
+	"yellow": 4,
+	"green":  5,
+	"blue":   6,
+	"violet": 7,
+	"grey":   8,
+	"white":  9,
+}
+
 // Colors should return the list of all colors.
 func Colors() []string {
-	panic("Please implement the Colors function")
+	keys := make([]string, 0, len(colors))
+
+	for key := range colors {
+		keys = append(keys, key)
+	}
+
+	sort.SliceStable(keys, func(i, j int) bool {
+		return colors[keys[i]] < colors[keys[j]]
+	})
+
+	return keys
 }
 
 // ColorCode returns the resistance value of the given color.
 func ColorCode(color string) int {
-	panic("Please implement the ColorCode function")
+	if resistance, ok := colors[color]; ok {
+		return resistance
+	}
+
+	return 0
 }
