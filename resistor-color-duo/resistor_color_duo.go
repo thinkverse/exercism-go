761a405aa4c56dad635040443f35ebde7c6c15b2
Kim Hallberg <hallberg.kim@gmail.com>
2023-03-15 18:06:29 +0100

    Update `Resistor Color Duo` exercise

--- resistor-color-duo/resistor_color_duo.go
+++ resistor-color-duo/resistor_color_duo.go
@@ -1,10 +1,5 @@
 package resistorcolorduo
 
-import (
-	"fmt"
-	"strconv"
-)
-
 var _colors = map[string]int{
 	"black":  0,
 	"brown":  1,
@@ -24,11 +19,5 @@ func Value(colors []string) int {
 		return 0
 	}
 
-	res, err := strconv.Atoi(fmt.Sprintf("%d%d", _colors[colors[0]], _colors[colors[1]]))
-
-	if err != nil {
-		return 0
-	}
-
-	return res
+	return (_colors[colors[0]] * 10) + _colors[colors[1]]
 }
