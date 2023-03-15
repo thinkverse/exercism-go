5e840066e49dd87d1a4db1e1ec1208d71ab64702
Kim Hallberg <hallberg.kim@gmail.com>
2023-03-15 19:24:26 +0100

    Complete `Sieve` exercise

--- sieve/sieve.go
+++ sieve/sieve.go
@@ -1,5 +1,25 @@
 package sieve
 
 func Sieve(limit int) []int {
-	panic("Please implement the Sieve function")
+	var primes []int
+
+	// Bool slice to cache prime numbers,
+	// mark all numbers first as primes.
+	cache := make([]bool, (limit + 1))
+	for i := 2; i <= limit; i++ {
+		cache[i] = true
+	}
+
+	for i := 2; i <= limit; i++ {
+		if cache[i] {
+			primes = append(primes, i)
+
+			// Mark composite numbers as non-primes.
+			for j := i * i; j <= limit; j += i {
+				cache[j] = false
+			}
+		}
+	}
+
+	return primes
 }
