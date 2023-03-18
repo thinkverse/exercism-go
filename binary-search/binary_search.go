68f1b748d294f7db0ae57f55db4d7d5c5a50660c
Kim Hallberg <hallberg.kim@gmail.com>
2023-03-18 19:02:45 +0100

    Update `Binary Search` exercise Small refactoring to make it easier to understand

--- binary-search/binary_search.go
+++ binary-search/binary_search.go
@@ -1,10 +1,7 @@
 package binarysearch
 
 func SearchInts(list []int, key int) int {
-	min := 0
-	max := len(list) - 1
-
-	for min <= max {
+	for min, max := 0, len(list); min < max; {
 		guess := (min + max) / 2
 
 		if list[guess] == key {
@@ -12,7 +9,7 @@ func SearchInts(list []int, key int) int {
 		} else if list[guess] < key {
 			min = guess + 1
 		} else {
-			max = guess - 1
+			max = guess
 		}
 	}
 
