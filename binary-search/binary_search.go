46172aabad8dd162c316538e5b98321d84f63e53
Kim Hallberg <hallberg.kim@gmail.com>
2023-03-18 17:35:32 +0100

    Complete `Binary Search` exercise

--- binary-search/binary_search.go
+++ binary-search/binary_search.go
@@ -1,5 +1,20 @@
 package binarysearch
 
 func SearchInts(list []int, key int) int {
-	panic("Please implement the SearchInts function")
+	min := 0
+	max := len(list) - 1
+
+	for min <= max {
+		guess := (min + max) / 2
+
+		if list[guess] == key {
+			return guess
+		} else if list[guess] < key {
+			min = guess + 1
+		} else {
+			max = guess - 1
+		}
+	}
+
+	return -1
 }
