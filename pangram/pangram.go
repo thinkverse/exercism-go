501abb7c8f032f2c860a92910c03d3c4b1c5ba31
Kim Hallberg <hallberg.kim@gmail.com>
2023-03-18 21:29:58 +0100

    Complete `Pangram` exercise

--- pangram/pangram.go
+++ pangram/pangram.go
@@ -1,5 +1,17 @@
 package pangram
 
 func IsPangram(input string) bool {
-	panic("Please implement the IsPangram function")
+	set := make(map[rune]bool, 26)
+
+	for _, char := range input {
+		if char <= 'Z' {
+			char += 1 << 5
+		}
+
+		if 'a' <= char && char <= 'z' {
+			set[char] = true
+		}
+	}
+
+	return len(set) == 26
 }
