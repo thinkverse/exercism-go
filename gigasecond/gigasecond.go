5465690aab2a1a213b4f4f5c7edf00ffde94767a
Kim Hallberg <hallberg.kim@gmail.com>
2023-03-26 01:53:46 +0100

    Complete `Gigasecond` exercise

--- gigasecond/gigasecond.go
+++ gigasecond/gigasecond.go
@@ -1,18 +1,8 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package gigasecond should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package gigasecond
 
-// import path for the time package from the standard library
 import "time"
 
-// AddGigasecond should have a comment documenting it.
+// AddGigasecond returns the time t+gigasecond.
 func AddGigasecond(t time.Time) time.Time {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return t
+	return t.Add(time.Second * 1e9)
 }
