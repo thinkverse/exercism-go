7ff814263f7a9f4421e87517038756b055296d29
Kim Hallberg <hallberg.kim@gmail.com>
2023-03-26 01:36:27 +0100

    Complete `Collatz Conjecture` exercise

--- collatz-conjecture/collatz_conjecture.go
+++ collatz-conjecture/collatz_conjecture.go
@@ -1,5 +1,21 @@
 package collatzconjecture
 
+import "errors"
+
 func CollatzConjecture(n int) (int, error) {
-	panic("Please implement the CollatzConjecture function")
+	if n <= 0 {
+		return -1, errors.New("n must be a positive integer")
+	}
+
+	var steps int
+
+	for ; n != 1; steps++ {
+		if (n % 2) == 0 {
+			n = n / 2
+		} else {
+			n = (n * 3) + 1
+		}
+	}
+
+	return steps, nil
 }
